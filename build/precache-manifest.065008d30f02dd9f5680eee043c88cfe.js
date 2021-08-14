a7352c0b0b78c7244c84504f911acb0a7f64b0a4
Ujjaval Sinha <39913625+ujjavalsinha@users.noreply.github.com>
2021-08-14 11:24:16 +0530

    Add files via upload

---
+++ build/precache-manifest.065008d30f02dd9f5680eee043c88cfe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3269aae4bf501bb7ab59429e67cbbab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03274f14c4f3faca76fe",
+    "url": "/static/css/2.50f53c61.chunk.css"
+  },
+  {
+    "revision": "5eda08a994fe9b6b46ec",
+    "url": "/static/css/main.70ea5631.chunk.css"
+  },
+  {
+    "revision": "03274f14c4f3faca76fe",
+    "url": "/static/js/2.19d0971e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.19d0971e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eda08a994fe9b6b46ec",
+    "url": "/static/js/main.5f627cb0.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  },
+  {
+    "revision": "b6fb3befcd33a1d45edb8c31452c5fb3",
+    "url": "/static/media/avatar.b6fb3bef.svg"
+  },
+  {
+    "revision": "f81649078de71177dfb3ee8b5cf9dc19",
+    "url": "/static/media/pen.f8164907.svg"
+  }
+]);
